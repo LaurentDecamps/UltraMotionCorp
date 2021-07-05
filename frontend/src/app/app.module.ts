bf45db09fa15466286824eb44cf6a40e4c55b7cd
Yassine Tissafi <yassine.tissafi@free.fr>
2021-07-05 11:29:13 +0200

    created pages and components folders

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccueilComponent } from './accueil/accueil.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
--- frontend/src/app/pages/accueil/accueil.component.ts
+++ frontend/src/app/pages/accueil/accueil.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProfessionelService } from '../services/professionel.service';
+import { ProfessionelService } from '../../services/professionel.service';
 
 @Component({
   selector: 'app-accueil',
