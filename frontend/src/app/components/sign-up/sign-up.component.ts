609f09271ef78802de594805b7d4bd79cf550691
Kévin <noxtariadev@gmail.com>
2021-07-20 10:52:32 +0200

    Ajout back Création Entreprise

--- frontend/src/app/components/sign-up/sign-up.component.ts
+++ frontend/src/app/components/sign-up/sign-up.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ClientService } from 'src/app/services/client.service';
+import { EntreprisesService } from 'src/app/services/entreprises.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -11,15 +12,26 @@ import { ClientService } from 'src/app/services/client.service';
 export class SignUpComponent implements OnInit {
 
   clientForm : FormGroup;
+  entrepriseForm : FormGroup;
 
   isDisplay = true;
   entrepriseIsDisplay = true;
   
 
-  constructor(private clientService : ClientService, private router : Router,private fb : FormBuilder) {
+  constructor(private clientService : ClientService, private entreprisesService : EntreprisesService, private router : Router,private fb : FormBuilder) {
     this.clientForm = this.fb.group({
       nom : "",
       prenom : "",
+      email : "",
+      numeroTelephone : 0,
+      motDePasse : "",
+      adresse : ""
+
+    })
+    this.entrepriseForm = this.fb.group({
+      nom : "",
+      description : "",
+      logoUri : "",
       email : "",
       numeroTelephone : 0,
       motDePasse : "",
@@ -39,9 +51,15 @@ export class SignUpComponent implements OnInit {
     this.entrepriseIsDisplay = !this.entrepriseIsDisplay;
   }
 
-  envoyer = () => {
+  envoyerClient = () => {
     console.log(this.clientForm.value);
     this.clientService.create(this.clientForm.value).subscribe( () => {
+      this.router.navigateByUrl("/mncpt");
+    })
+  }
+  envoyerEntreprise = () => {
+    console.log(this.entrepriseForm.value);
+    this.entreprisesService.create(this.entrepriseForm.value).subscribe( () => {
       this.router.navigateByUrl("/mncpt");
     })
   }
