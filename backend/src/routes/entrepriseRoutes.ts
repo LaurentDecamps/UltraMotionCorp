10ee68a1531d9c6fb1aeafb322bab151f71214ec
Décamps Laurent <decamps.laurent@gmail.com>
2021-07-26 01:41:55 +0200

    Modification de la création de projet, déplacement et mise à jour de la partie inscription

--- backend/src/routes/entrepriseRoutes.ts
+++ backend/src/routes/entrepriseRoutes.ts
@@ -4,7 +4,7 @@ import {entrepriseController} from "../controllers/entrepriseController"
 const endpoint = "entreprises"
 
 export const setEntrepriseRouting = (app) => {
-  app.get(`/${endpoint}`,  verifyToken, entrepriseController.findAll)
+  app.get(`/${endpoint}`, entrepriseController.findAll)
   app.get(`/${endpoint}/:id`,  verifyToken, entrepriseController.findById)
   app.post(`/${endpoint}`,  verifyToken, entrepriseController.create)
   app.put(`/${endpoint}/:id`,  verifyToken, entrepriseController.update)
