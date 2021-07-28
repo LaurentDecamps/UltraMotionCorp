164867ca5bb59aea9d59b84783038aa9e05e7832
Décamps Laurent <decamps.laurent@gmail.com>
2021-07-28 14:59:16 +0200

    Correction de l'accès aux évaluations
    Correction de l'accès aux évaluations

--- frontend/src/app/components/evaluations/evaluations.component.ts
+++ frontend/src/app/components/evaluations/evaluations.component.ts
@@ -1,6 +1,7 @@
 import { Evaluation } from './../../models/evaluations';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { EvaluationsService } from 'src/app/services/evaluations.service';
+import { AuthentificationService } from 'src/app/services/authentification.service';
 
 @Component({
   selector: 'app-evaluations',
@@ -14,11 +15,14 @@ export class EvaluationsComponent implements OnInit {
 
   @Output() transmettreDemandeEvaluation = new EventEmitter<any>();
 
-  constructor(private evaluationsService: EvaluationsService) {}
+  constructor(private evaluationsService: EvaluationsService,
+    private authentificationService: AuthentificationService) {}
 
   ngOnInit(): void {
+    console.log("Client",this.authentificationService.currentClientValue.client.id);
+
     this.evaluationsService
-      .getEvaluationByClient(localStorage.getItem('clientCourant'))
+      .getEvaluationByClient(this.authentificationService.currentClientValue?.client.id)
       .subscribe((data) => {
         this.evaluations = data;
       });
--- frontend/src/app/components/infos/infos.component.ts
+++ frontend/src/app/components/infos/infos.component.ts
@@ -54,4 +54,7 @@ export class InfosComponent implements OnInit {
     this.authentificationService.logout();
   }
 
+  ngAfterContentInit() {
+    localStorage.removeItem("creationCompteReussi");
+  }
 }
